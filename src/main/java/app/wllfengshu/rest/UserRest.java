92ec7fade481df4d01d41e8d809db8f0e99f07ef
wllfengshu <wllfengshu@163.com>
2018-04-21 20:04:53 +0800

    sessionid的i大写

--- src/main/java/app/wllfengshu/rest/UserRest.java
+++ src/main/java/app/wllfengshu/rest/UserRest.java
@@ -38,7 +38,7 @@ public class UserRest {
 	 * @return
 	 */
     @GET
-    public Response getusers(@HeaderParam(value="sessionid") String sessionId,
+    public Response getusers(@HeaderParam(value="sessionId") String sessionId,
     		@Context HttpServletRequest request,@Context HttpServletResponse response) {
 		String responseStr = null;
 		try{
@@ -65,7 +65,7 @@ public class UserRest {
      */
     @POST
     public Response adduser(String user,
-    		@HeaderParam(value="sessionid") String sessionId,
+    		@HeaderParam(value="sessionId") String sessionId,
     		@Context HttpServletRequest request,@Context HttpServletResponse response) {
 		String responseStr = null;
 		try{
@@ -93,7 +93,7 @@ public class UserRest {
     @GET
     @Path("/{user_id}/")
     public Response getuser(@PathParam("user_id")String user_id,
-    		@HeaderParam(value="sessionid") String sessionId,
+    		@HeaderParam(value="sessionId") String sessionId,
     		@Context HttpServletRequest request,@Context HttpServletResponse response) {
 		String responseStr = null;
 		try{
@@ -121,7 +121,7 @@ public class UserRest {
     @PUT
     @Path("/{user_id}/")
     public Response updateuser(String user,
-    		@HeaderParam(value="sessionid") String sessionId,
+    		@HeaderParam(value="sessionId") String sessionId,
     		@Context HttpServletRequest request,@Context HttpServletResponse response) {
 		String responseStr = null;
 		try{
@@ -149,7 +149,7 @@ public class UserRest {
     @DELETE
     @Path("/{user_id}/")
     public Response deleteuser(@PathParam("user_id")String user_id,
-    		@HeaderParam(value="sessionid") String sessionId,
+    		@HeaderParam(value="sessionId") String sessionId,
     		@Context HttpServletRequest request,@Context HttpServletResponse response) {
 		String responseStr = null;
 		try{
